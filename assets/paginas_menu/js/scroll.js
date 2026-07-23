0f22ad963d2712c7b52567afa2f44017c86b09fa
IGVS-UI <igorgomessilva128@gmail.com>
2026-07-22 23:53:14 -0300

    page tecnologic, congratulations

--- assets/paginas_menu/js/scroll.js
+++ assets/paginas_menu/js/scroll.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     const items = document.querySelectorAll(
-        ".frase-destaque h2 span, .hero-tec .hero-tec-text, .hero-tec .caixa_de_texto, .hero-tec .Mulher_vr, .vr, .vr-card, .vr-image, .vr-text, .AR,.caixa_de_texto_AR, .vr-image_AR"
+        ".frase-destaque h2 span, .hero-tec .hero-tec-text, .hero-tec .caixa_de_texto, .hero-tec .Mulher_vr, .vr, .vr-card, .vr-image, .vr-text, .AR, .caixa_de_texto_AR, .vr-image_AR, .ARs, .imgs, .caixa_de_texto_ARs, .AR-image1, .AR-image2, .AR-image3, .button_explor, .btn-Explorar, .MR, .caixa_de_texto_MR, .holograma, #frase-destaque"
     );
     items.forEach((el) => observer.observe(el));
 });
